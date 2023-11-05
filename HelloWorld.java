97d55b7bc2e3f7236d99e59470739e919b9f76ce
Irina Jovanoska <irinajovanoska1414@gmail.com>
2023-11-05 14:20:38 +0100

    Second commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main (String [] args) {
         System.out.println ("Hello World OOP FIKT");
+        //https://github.com/IrinaJova/OOP.git
     }
 
 }
